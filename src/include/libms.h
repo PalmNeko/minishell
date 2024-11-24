0089f1b57f2039e83ebea53d5cdd589995d2f045
PalmNeko <nitunamoni13579@gmail.com>
2024-11-24 23:34:29 +0900

    add: environ functions

--- src/include/libms.h
+++ src/include/libms.h
@@ -38,4 +38,11 @@ char	**ms_dup_ntp(const char *ntp[]);
 char	**ms_dup_ntp2(const char *const ntp[]);
 void	ms_destroy_ntp(char *ntp[]);
 
+// environment variable
+char	*ms_getenv(const char *name);
+int		ms_setenv(const char *name, const char *value, int overwrite);
+int		ms_unsetenv(const char *name);
+int		ms_import_env(char *const envp[]);
+char	**ms_export_env(void);
+
 #endif
