7ee1fab71f22afb4051dfd8c0b039edda2ea7975
nytsdayo <nyantsutsu@gmail.com>
2025-01-15 03:37:56 +0900

    init 2

--- src/include/semantic_analyze.h
+++ src/include/semantic_analyze.h
@@ -1,5 +1,9 @@
 #ifndef SEMANTIC_ANALYZE_H
 #define SEMANTIC_ANALYZE_H
 
-// t_lsa *semantic_analyze(t_syntax_node *node);
+#include "syntax_analyze_type.h"
+#include "semantic_analyze_type.h"
+
+t_lsa *semantic_analyze(t_syntax_node *node);
+
 #endif
--- src/semantic_analyze/semantic_analyze.c
+++ src/semantic_analyze/semantic_analyze.c
@@ -1,9 +1,9 @@
 #include "semantic_analyze.h"
-// #include "syntax_analyze.h"
 
-void semantic_analyze(){
-	return ;
+t_lsa *semantic_analyze(t_syntax_node *node){
+	t_lsa *lsa = malloc(sizeof(t_lsa));
+	if (lsa == NULL)
+		return (NULL);
+	lsa->lists = NULL;
+	return (lsa);
 }
-// t_lsa *semantic_analyze(t_syntax_node *node){
-
-// }
