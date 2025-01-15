8aab5744d9bf74fd33d2d3f4fafe2305dcbfd173
PalmNeko <54460913+PalmNeko@users.noreply.github.com>
2025-01-15 17:10:51 +0900

    add: ms_setup (#66)

---
+++ src/include/setup.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setup.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 13:30:02 by tookuyam          #+#    #+#             */
+/*   Updated: 2025/01/15 13:46:31 by tookuyam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SETUP_H
+# define SETUP_H
+
+# include "libft.h"
+# include "setup_type.h"
+
+t_minishell	*ms_setup(void);
+void		ms_cleanup_and_exit(int status);
+
+#endif
