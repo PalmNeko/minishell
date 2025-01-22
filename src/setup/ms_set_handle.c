7c96cca0f00de9c19f2a41e138181a33488bd3a6
PalmNeko <nitunamoni13579@gmail.com>
2025-01-22 09:16:05 +0000

    fix: signal error

--- src/setup/ms_set_handle.c
+++ src/setup/ms_set_handle.c
@@ -6,19 +6,27 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 05:55:29 by tookuyam          #+#    #+#             */
-/*   Updated: 2025/01/19 11:18:50 by tookuyam         ###   ########.fr       */
+/*   Updated: 2025/01/22 09:14:33 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "signal_header.h"
 #include <readline/readline.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-static void	ms_handle_sigint(int sig);
+int	g_rl_is_sigint;
+
 static void	set_signal_handler(int sig, void (*handler)(int));
+static int	ms_startup_hook_handler(void);
+static int	ms_event_hook_handler(void);
+static void	ms_handle_sigint(int sig);
 
 int	ms_set_handle(void)
 {
+	rl_startup_hook = ms_startup_hook_handler;
+	rl_event_hook = ms_event_hook_handler;
 	set_signal_handler(SIGINT, ms_handle_sigint);
 	set_signal_handler(SIGQUIT, SIG_IGN);
 	return (0);
@@ -34,11 +42,28 @@ void	set_signal_handler(int sig, void (*handler)(int))
 	sigaction(sig, &act, NULL);
 }
 
-void	ms_handle_sigint(int sig)
+static int	ms_startup_hook_handler(void)
 {
-	(void)sig;
-	rl_replace_line("", 0);
-	printf("\n");
-	rl_on_new_line();
-	rl_redisplay();
+	g_rl_is_sigint = 0;
+	return (0);
+}
+
+static int	ms_event_hook_handler(void)
+{
+	if (g_rl_is_sigint)
+	{
+		rl_replace_line("", 0);
+		rl_done = 1;
+	}
+	return (0);
+}
+
+static void	ms_handle_sigint(int sig)
+{
+	if (sig == SIGINT)
+	{
+		rl_replace_line("", 0);
+		g_rl_is_sigint = 1;
+		rl_done = 1;
+	}
 }
