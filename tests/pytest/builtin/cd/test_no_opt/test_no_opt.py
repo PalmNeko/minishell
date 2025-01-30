da880b7e2724147547e1f5e4b26358a50a0d1ad5
PalmNeko <nitunamoni13579@gmail.com>
2025-01-30 21:27:02 +0000

    add: test cd: no opt

---
+++ tests/pytest/builtin/cd/test_no_opt/test_no_opt.py
@@ -0,0 +1,10 @@
+import script_tester
+import pytest
+
+# @pytest.mark.skip(reason="デバッグの時用")
+def test_basic_usage():
+    script_tester.test("builtin/cd/test_no_opt/can_change_directory.sh",
+		expected_stdout= (
+			"/app/tests/pytest/builtin/cd/test_no_opt/physical_dir\n"
+			"/app/tests/pytest/builtin/cd/test_no_opt/symbolic_dir\n"
+			))
