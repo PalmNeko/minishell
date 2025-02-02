cffa933efda7ed8738f7d90c32452507730dc3f8
nytsdayo <nyantsutsu@gmail.com>
2025-02-02 18:51:48 +0900

    fix tokenize_word

--- src/lexical_analyze/tokenize/ms_tokenize_word.c
+++ src/lexical_analyze/tokenize/ms_tokenize_word.c
@@ -6,7 +6,7 @@
 /*   By: rnakatan <rnakatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 07:38:45 by rnakatan          #+#    #+#             */
-/*   Updated: 2025/02/02 06:56:13 by rnakatan         ###   ########.fr       */
+/*   Updated: 2025/02/02 18:48:31 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_token	*ms_tokenize_word(const char *input, int pos)
 	{
 		while (input[pos] && ms_is_word_token_char(&input[pos]))
 			pos++;
-		token = ms_create_token(TK_WORD, input, start_pos, pos);
+		token = ms_create_token(TK_WORD, &input[start_pos], start_pos, pos);
 	}
 	else
 		token = ms_create_token(TK_DECLINED, "", pos, pos);
@@ -35,7 +35,7 @@ bool ms_is_word_token_char(const char *str)
 {
 	if (ft_includes(*str, NOT_WORD_CHARS))
 	{
-		if(*str == '&' && ft_strncmp(str, "&&", 2) == 0)
+		if(*str == '&' && ft_strncmp(str, "&&", 2) != 0)
 			return (1);
 		else
 			return (0);
