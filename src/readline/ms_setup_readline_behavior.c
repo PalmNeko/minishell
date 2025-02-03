22d46b67f0d6066e8709364df2c9d7adb2030307
PalmNeko <nitunamoni13579@gmail.com>
2025-02-03 10:48:27 +0000

    fix: readline setup for behavior

--- src/readline/ms_setup_readline_behavior.c
+++ src/readline/ms_setup_readline_behavior.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 13:18:44 by tookuyam          #+#    #+#             */
-/*   Updated: 2025/01/30 20:13:41 by tookuyam         ###   ########.fr       */
+/*   Updated: 2025/02/03 10:41:39 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 #include "readline.h"
 #include <readline/readline.h>
 #include <unistd.h>
-#include <termio.h>
-
-static void	ms_disable_echo(int meta_flag);
-static void	ms_enable_echo(void);
+#include <termios.h>
 
 int	ms_setup_readline_behavior(void)
 {
@@ -25,27 +22,8 @@ int	ms_setup_readline_behavior(void)
         ms_setup_readline_handler();
     else
     {
-        rl_prep_term_function = ms_disable_echo;
-        rl_deprep_term_function = ms_enable_echo;
+        rl_prep_term_function = NULL;
+        rl_deprep_term_function = NULL;
     }
 	return (0);
-}
-
-static void	ms_disable_echo(int meta_flag)
-{
-    struct termios tty;
-
-	(void)meta_flag;
-    tcgetattr(STDIN_FILENO, &tty);
-    tty.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-}
-
-static void	ms_enable_echo(void)
-{
-    struct termios tty;
-
-    tcgetattr(STDIN_FILENO, &tty);
-    tty.c_lflag |= ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
 }
