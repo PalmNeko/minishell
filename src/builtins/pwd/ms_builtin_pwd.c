4fdd42deb76f763e93af35588cc758d0e10d63e7
PalmNeko <nitunamoni13579@gmail.com>
2025-02-03 06:22:33 +0000

    update: ms_builtin_pwd for new option parser

--- src/builtins/pwd/ms_builtin_pwd.c
+++ src/builtins/pwd/ms_builtin_pwd.c
@@ -6,13 +6,14 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 13:53:39 by tookuyam          #+#    #+#             */
-/*   Updated: 2025/02/01 08:46:40 by tookuyam         ###   ########.fr       */
+/*   Updated: 2025/02/03 06:21:32 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin_internal.h"
 #include "libft.h"
 #include "libms.h"
+#include "ms_getopt.h"
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -22,21 +23,17 @@ static void	ms_print_usage_pwd(void);
 int	ms_builtin_pwd(
 		const char *path, char *const argv[], char *const envp[])
 {
-	char	**arg_head;
-	char	*invalid_opt;
-	char	*cwd;
-	char	opt_txt[3];
+	char		*cwd;
+	t_opting	opting;
+	int			argc;
 
 	(void)path;
 	(void)envp;
-	arg_head = ms_get_argument_head(argv, "");
-	if (arg_head == NULL)
-		return (1);
-	invalid_opt = ms_get_first_invalid_opt(argv, "");
-	if (invalid_opt != NULL)
+	argc = (int)ms_ntpsize((void **)argv);
+	ms_getopt_init(&opting, argc, (char **)argv, "");
+	while (ms_getopt_parse(&opting))
 	{
-		ms_set_opt_txt(opt_txt, invalid_opt[0]);
-		ms_perror_cmd2("pwd", opt_txt, "invalid option");
+		ms_perror_invalid_option("pwd", opting.optopt);
 		ms_print_usage_pwd();
 		return (2);
 	}
