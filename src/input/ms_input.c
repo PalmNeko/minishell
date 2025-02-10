26e6cf041b33abc9e70834d13e37d7e0b034a6a7
PalmNeko <54460913+PalmNeko@users.noreply.github.com>
2025-02-11 03:36:57 +0900

    fix: fogot unclosed quote (#103)

--- src/input/ms_input.c
+++ src/input/ms_input.c
@@ -19,6 +19,7 @@
 #include <stdlib.h>
 
 static char	*ms_get_user_input(t_minishell mnsh);
+static bool	ms_is_quoted_closed(const char *line);
 
 void	ms_input(t_minishell mnsh)
 {
@@ -58,10 +59,32 @@ static char	*ms_get_user_input(t_minishell mnsh)
 	}
 	if (ms_replace_joined_str(&line, "\n") == NULL)
 		return (free(line), NULL);
+	if (!ms_is_quoted_closed(line))
+		return (line);
 	heredoc_input = ms_input_heredoc(mnsh, line);
 	if (heredoc_input == NULL)
 		return (NULL);
 	if (ms_replace_joined_str(&line, heredoc_input) == NULL)
 		return (free(line), free(heredoc_input), NULL);
 	return (line);
+}
+
+static bool	ms_is_quoted_closed(const char *line)
+{
+	char	*first_line_end;
+
+	first_line_end = ft_strchr(line, '\n');
+	if (first_line_end == NULL)
+		return (false);
+	while (*line != '\0')
+	{
+		if (ft_includes(*line, "\"'"))
+		{
+			line = ft_strchr(line + 1, *line);
+			if (line == NULL || line > first_line_end)
+				return (false);
+		}
+		line++;
+	}
+	return (true);
 }
