b065b1cca0422f5bd1ae66ba9c84d6d4a2738596
nytsdayo <nyantsutsu@gmail.com>
2025-03-15 19:25:25 +0900

    apply_norm

--- src/syntax_analyze/parse/ms_parse_command.c
+++ src/syntax_analyze/parse/ms_parse_command.c
@@ -6,7 +6,7 @@
 /*   By: rnakatan <rnakatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 19:38:38 by rnakatan          #+#    #+#             */
-/*   Updated: 2025/02/02 19:38:38 by rnakatan         ###   ########.fr       */
+/*   Updated: 2025/03/15 19:24:51 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,53 +14,66 @@
 #include "syntax_analyze.h"
 #include <stdlib.h>
 
+static t_syntax_node	*ms_parse_command_tail(t_token **tokens, int pos);
+
+static const t_parse_func	g_ms_command_head_func_list[3] = {
+	ms_parse_assignment_command,
+	ms_parse_simple_command,
+	NULL
+};
+
+static const t_syntax_rule	*g_ms_command_rule_list[] = {
+	(t_syntax_rule []){{
+	EBNF_ONE,
+	(t_parse_func *)g_ms_command_head_func_list,
+	false,
+	true}},
+	(t_syntax_rule []){{
+	EBNF_REPEAT,
+	(t_parse_func[]){
+	ms_parse_command_tail,
+	NULL},
+	true,
+	true}},
+	NULL
+};
+
+static const t_syntax_rule	*g_ms_command_tail_rule_list[] = {
+	(t_syntax_rule []){{
+	EBNF_OPTION,
+	(t_parse_func[]){
+	ms_parse_blank,
+	NULL},
+	false,
+	false}},
+	(t_syntax_rule []){{
+	EBNF_ONE,
+	(t_parse_func[]){
+	ms_parse_simple_command,
+	NULL},
+	false,
+	true}},
+	NULL
+};
+
 t_syntax_node	*ms_parse_command(t_token **tokens, int pos)
 {
 	t_syntax_node		*node;
-	t_syntax_node		*child;
-	t_syntax_node_list	*child_lst;
-	const int			start_pos = pos;
-	bool				is_blank;
 
-	is_blank = false;
-	child_lst = NULL;
-	// call mkmemscorp
-	child = ms_parse_assignment_command(tokens, pos);
-	if (child == NULL)
-		return (NULL);
-	if (child->type == SY_DECLINED)
-		ms_syntax_node_destroy(child);
-	else
-	{
-		ms_lstappend_tail(&child_lst, child, ms_syntax_node_destroy_wrapper);
-		if (child_lst == NULL)
-			return (ms_syntax_node_destroy(child), NULL);
-		pos = child->end_pos;
-	}
-	is_blank = (tokens[pos] && tokens[pos]->type == TK_BLANK);
-	while (tokens[pos + is_blank])
-	{
-		child = ms_parse_simple_command(tokens, pos + is_blank);
-		if (child == NULL)
-			return (ft_lstclear(&child_lst, ms_syntax_node_destroy_wrapper),
-				NULL);
-		if (child->type == SY_DECLINED)
-		{
-			ms_syntax_node_destroy(child);
-			if (child_lst == NULL)
-				return (ms_parse_declined(tokens, pos + is_blank));
-			break ;
-		}
-		ms_lstappend_tail(&child_lst, child, ms_syntax_node_destroy_wrapper);
-		if (child_lst == NULL)
-			return (ft_lstclear(&child_lst, ms_syntax_node_destroy_wrapper),
-				NULL);
-		pos = child->end_pos;
-		is_blank = (tokens[pos] && tokens[pos]->type == TK_BLANK);
-	}
-	node = ms_syntax_node_create_nonterminal(SY_COMMAND, &child_lst, start_pos,
-			pos);
+	node = ms_parse_rules(tokens, pos, SY_COMMAND,
+			(t_syntax_rule **)g_ms_command_rule_list);
 	if (node == NULL)
-		return (ft_lstclear(&child_lst, ms_syntax_node_destroy_wrapper), NULL);
+		return (NULL);
+	return (node);
+}
+
+static t_syntax_node	*ms_parse_command_tail(t_token **tokens, int pos)
+{
+	t_syntax_node	*node;
+
+	node = ms_parse_rules(tokens, pos, SY_ACCEPTED,
+			(t_syntax_rule **)g_ms_command_tail_rule_list);
+	if (node == NULL)
+		return (NULL);
 	return (node);
 }
