53445992db255f2b28f2f2d8c18d0fa78bff5b11
PalmNeko <nitunamoni13579@gmail.com>
2025-03-19 14:24:33 +0000

    fix: non-interactive history export and import

--- src/setup/ms_setup_history.c
+++ src/setup/ms_setup_history.c
@@ -6,18 +6,21 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 12:14:00 by tookuyam          #+#    #+#             */
-/*   Updated: 2025/01/18 18:04:37 by tookuyam         ###   ########.fr       */
+/*   Updated: 2025/03/19 14:16:44 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libms.h"
 #include "history.h"
+#include "setup.h"
 #include <stdio.h>
 
 int	ms_setup_history(void)
 {
 	char	*filename;
 
+	if (ms_is_interactive() == false)
+		return (0);
 	filename = ms_getenv("HISTFILE");
 	if (filename == NULL)
 		filename = "~/.mnsh_history";
