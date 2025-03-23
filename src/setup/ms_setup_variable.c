223393b4ef27c1def17d138280cfdf54897243b4
PalmNeko <nitunamoni13579@gmail.com>
2025-03-23 02:27:56 +0000

    add: test and code for initial status variable

--- src/setup/ms_setup_variable.c
+++ src/setup/ms_setup_variable.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 17:29:38 by tookuyam          #+#    #+#             */
-/*   Updated: 2025/02/03 08:06:05 by tookuyam         ###   ########.fr       */
+/*   Updated: 2025/03/23 02:23:00 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ void	ms_setup_variable(void)
 	cwd = ms_get_current_dir_name();
 	if (cwd != NULL)
 		ms_setenv("PWD", cwd, 1);
+	ms_setenv("?", "0", 0);
 	free(cwd);
 	if (ms_is_interactive() == true)
 		ms_setup_only_interactive_var();
