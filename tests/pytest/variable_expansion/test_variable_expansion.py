cc9a45ec5f36c218c86c0cd9bb194596ec819109
nytsdayo <nyantsutsu@gmail.com>
2025-03-24 21:25:00 +0900

    add test : variableの基本的なテストを追加

---
+++ tests/pytest/variable_expansion/test_variable_expansion.py
@@ -0,0 +1,21 @@
+import script_tester
+import pytest
+import os
+
+def test_basic():
+	result = os.path.expandvars("$PATH") + "\n"
+	result2 = os.path.expandvars("$HOME") + "\n"
+	script_tester.test("variable_expansion/test_basic.sh",
+		expected_stdout = result + result2,
+		expected_stderr = ""
+	)
+
+def test_assignment():
+	result = "simple_value" + "\n"
+	result2 = "value  include   space" + "\n"
+	result3 = "join double quoted & non_quoted & single quoted" + "\n"
+	result4 = "assignment($HOGE=hoge) self three time. hogehogehoge" + "\n"
+	script_tester.test("variable_expansion/test_assignment.sh",
+		expected_stdout = result + result2 + result3 + result4,
+		expected_stderr = ""
+	)
