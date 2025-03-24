5a8626d0389b4b8fe93e967d223e54d94bddda9f
PalmNeko <nitunamoni13579@gmail.com>
2025-03-24 01:59:49 +0000

    fix: sigsegv if unset home variable

--- src/builtins/cd/ms_get_cd_path.c
+++ src/builtins/cd/ms_get_cd_path.c
@@ -16,24 +16,28 @@
 #include <errno.h>
 #include <string.h>
 
-char	*ms_get_cd_path(const char *arg_path)
+int	ms_get_cd_path(const char *arg_path, char **cd_path)
 {
 	char	*target_pathname;
 
 	target_pathname = NULL;
+	*cd_path = NULL;
 	if (arg_path == NULL)
 	{
 		target_pathname = ms_getenv("HOME");
 		if (target_pathname == NULL)
-			ms_perror_cmd("cd", "HOME not set");
+			return (ms_perror_cmd("cd", "HOME not set"), 1);
 	}
 	else if (ft_strcmp(arg_path, "-") == 0)
 	{
 		target_pathname = ms_getenv("OLDPWD");
 		if (target_pathname == NULL)
-			ms_perror_cmd("cd", "OLDPWD not set");
+			return (ms_perror_cmd("cd", "OLDPWD not set"), 1);
 	}
 	else
 		target_pathname = (char *)arg_path;
-	return (ft_strdup(target_pathname));
+	*cd_path = ft_strdup(target_pathname);
+	if (*cd_path == NULL)
+		return (-1);
+	return (0);
 }
