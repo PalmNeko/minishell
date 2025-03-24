9c5da48b0154e8f3c276854b7b1a80b626bea331
PalmNeko <54460913+PalmNeko@users.noreply.github.com>
2025-03-24 12:29:40 +0900

    add: test and code for initial status variable (#166)

--- tests/pytest/general/test_command/test_command.py
+++ tests/pytest/general/test_command/test_command.py
@@ -6,3 +6,9 @@ def test_invalid_option():
         expected_stdout = "127\n",
         expected_stderr = (
             "minishell: A: command not found\n"))
+
+def test_initial_status():
+    script_tester.test("general/test_command/test_status.sh",
+        expected_stdout = "0\n",
+        expected_stderr = (
+            ""))
