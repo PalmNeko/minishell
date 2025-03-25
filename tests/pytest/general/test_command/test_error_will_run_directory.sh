fd0f3e62eac31733e819fce9d57c52a6080b099b
PalmNeko <nitunamoni13579@gmail.com>
2025-03-25 09:38:02 +0000

    fix: run with directory

---
+++ tests/pytest/general/test_command/test_error_will_run_directory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# テスト用の設定の読み込み
+cd "$(dirname $0)"
+. "../../test_prepare.sh"
+
+# テスト
+LEAKCHECK="valgrind -q --error-exitcode=12 --leak-check=full"
+$LEAKCHECK $PROG << "EOF"
+./
+echo $?
+EOF
