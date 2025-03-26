3982a7671f76fb20d786a795b3a3b4a3275c3746
PalmNeko <nitunamoni13579@gmail.com>
2025-03-26 04:43:19 +0000

    add: env-command test module

--- tests/googletest/builtins/env-command/test_ms_builtin_env.cpp
+++ tests/googletest/builtins/env-command/test_ms_builtin_env.cpp
@@ -9,6 +9,50 @@ extern "C" {
 	#include <sys/types.h>
 };
 
+typedef std::tuple<std::string, std::string, int> TestResult;
+
+/**
+ * return stdout stderr status
+ */
+std::tuple<std::string, std::string, int> test_runner_of_builtin_env(char **args, char **envp)
+{
+
+	int status;
+	std::string stdOut;
+	std::string stdErr;
+	std::tuple<std::string, std::string, int> t;
+
+	// 環境変数の設定
+	ms_clear_exports();
+	while (envp != NULL && *envp != NULL)
+	{
+		char	*name;
+		char	*nameend;
+		char	*value;
+
+		nameend = ft_strchr(*envp, '=');
+		if (nameend != NULL)
+		{
+			name = ft_strndup(*envp, nameend - *envp);
+			value = ft_strdup(nameend + 1);
+			ms_setenv(name, value, 1);
+			ms_add_export(name);
+			free(name);
+			free(value);
+		}
+		envp++;
+	}
+
+	// 実行
+	testing::internal::CaptureStdout();
+	testing::internal::CaptureStderr();
+	status = ms_builtin_env(NULL, args, NULL);
+	stdOut = testing::internal::GetCapturedStdout();
+	stdErr = testing::internal::GetCapturedStderr();
+	t = std::make_tuple(stdOut, stdErr, status);
+	return t;
+}
+
 /** envコマンドと一致することを確認する */
 TEST(ms_builtin_env, equal_env_command)
 {
