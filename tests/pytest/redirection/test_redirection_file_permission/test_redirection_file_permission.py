f086c94a3d8d5298ad1acfc0878193c8f4fb3fe1
PalmNeko <54460913+PalmNeko@users.noreply.github.com>
2025-03-27 17:56:48 +0900

    155 bug ファイル作成時のモードについて (#183)
    * fix: redirection permission
    
    * fix: memory leak
    
    * add: redirection file permission test
    
    * update: suppression file
    
    * fix: test case

---
+++ tests/pytest/redirection/test_redirection_file_permission/test_redirection_file_permission.py
@@ -0,0 +1,13 @@
+import script_tester
+import pytest
+import subprocess
+
+def test_create_permission():
+    script_tester.test("redirection/test_redirection_file_permission/test_create_permission.sh",
+        expected_stdout = "-rw-rw-rw-\n",
+        expected_stderr = "")
+
+def test_append_permission():
+    script_tester.test("redirection/test_redirection_file_permission/test_append_permission.sh",
+        expected_stdout = "-rw-rw-rw-\n",
+        expected_stderr = "")
