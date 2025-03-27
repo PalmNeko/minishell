f75003b5bde48b674e4e66fd84928a32455aa093
PalmNeko <nitunamoni13579@gmail.com>
2025-03-27 07:47:49 +0000

    fix: test case

--- tests/pytest/redirection/test_redirection_file_permission/test_redirection_file_permission.py
+++ tests/pytest/redirection/test_redirection_file_permission/test_redirection_file_permission.py
@@ -4,10 +4,10 @@ import subprocess
 
 def test_create_permission():
     script_tester.test("redirection/test_redirection_file_permission/test_create_permission.sh",
-        expected_stdout = "-rw-rw-rw\n",
+        expected_stdout = "-rw-rw-rw-\n",
         expected_stderr = "")
 
 def test_append_permission():
     script_tester.test("redirection/test_redirection_file_permission/test_append_permission.sh",
-        expected_stdout = "-rw-rw-rw\n",
+        expected_stdout = "-rw-rw-rw-\n",
         expected_stderr = "")
