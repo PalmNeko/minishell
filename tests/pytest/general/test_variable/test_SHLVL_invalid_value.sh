7b9d2491f3feb94264430bf6f5c30b1da411453c
nytsdayo <nyantsutsu@gmail.com>
2025-03-27 17:34:35 +0900

    add: SHLVL test (basic, invalid, undefind)

---
+++ tests/pytest/general/test_variable/test_SHLVL_invalid_value.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# テスト用の設定の読み込み
+cd "$(dirname $0)"
+. "../../test_prepare.sh"
+
+# テスト
+LEAKCHECK="valgrind -q --error-exitcode=12 --leak-check=full"
+TEMP=$SHLVL
+SHLVL=hogehoge
+$LEAKCHECK $PROG << "EOF"
+echo $SHLVL
+EOF
+SHLVL=$TEMP
--- tests/pytest/general/test_variable/test_variable.py
+++ tests/pytest/general/test_variable/test_variable.py
@@ -2,6 +2,12 @@ import script_tester
 import pytest
 import subprocess
 
+def test_SHLVL_basic():
+    base_shlvl = int(subprocess.run(['echo $SHLVL'], shell=True, capture_output=True, text=True).stdout) + 1
+    script_tester.test("general/test_variable/test_SHLVL_basic.sh",
+    expected_stdout = str(base_shlvl + 1) + "\n",
+    expected_stderr = "")
+
 def test_SHLVL_overflow_loop():
     script_tester.test("general/test_variable/test_SHLVL_overflow_loop.sh",
         expected_stdout = "3\n",
@@ -21,4 +27,20 @@ def test_SHLVL_boundary_value():
 			"0\n"),
         expected_stderr = (
 			"minishell: warning: shell level (2147483647) too high, resetting to 1\n"
+			))
+
+def test_SHLVL_undefind():
+    script_tester.test("general/test_variable/test_SHLVL_undefind.sh",
+        expected_stdout = (
+			"1\n"),
+        expected_stderr = (
+			""
+			))
+
+def test_SHLVL_invalid_value():
+    script_tester.test("general/test_variable/test_SHLVL_invalid_value.sh",
+        expected_stdout = (
+			"1\n"),
+        expected_stderr = (
+			""
 			))
