1423d125f0b35b42e0b231fc4d0ae7d473f04c9b
PalmNeko <nitunamoni13579@gmail.com>
2025-04-02 10:12:09 +0000

    add: test of ambiguous wildcard error

--- tests/pytest/redirection/test_redirection_error/test_redirection_error.py
+++ tests/pytest/redirection/test_redirection_error/test_redirection_error.py
@@ -13,3 +13,15 @@ def test_redirection_ambiguous_error_variable():
             "minishell: $Bob: ambiguous redirect\n"
             "minishell: $Chris: ambiguous redirect\n"
             ))
+
+def test_redirection_ambiguous_error_wildcard():
+    script_tester.test("redirection/test_redirection_error/test_redirection_ambiguous_error_wildcard.sh",
+		expected_stdout = (
+            "1\n"
+            "1\n"
+            "1\n"),
+        expected_stderr = (
+            "minishell: *: ambiguous redirect\n"
+            "minishell: *: ambiguous redirect\n"
+            "minishell: *: ambiguous redirect\n"
+            ))
