10cb839e7e6ba420a4d6d6b5e521c8228a0a674a
PalmNeko <nitunamoni13579@gmail.com>
2025-04-05 06:06:45 +0000

    add: escape cd path

--- src/builtins/cd/ms_perror_cd.c
+++ src/builtins/cd/ms_perror_cd.c
@@ -13,8 +13,44 @@
 #include "libms.h"
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-void	ms_perror_cd(const char *type)
+static bool	ms_is_should_escape(const char *path);
+
+void	ms_perror_cd(const char *path)
 {
-	ms_perror_cmd2("cd", type, strerror(errno));
+	char	*escaped_path;
+	char	*path_string;
+
+	if (ms_is_should_escape(path) == false)
+		ms_perror_cmd2("cd", path, strerror(errno));
+	else
+	{
+		path_string = ft_strdup("$'");
+		if (path_string == NULL)
+			return ;
+		escaped_path = ms_dup_escapedstr(path);
+		if (escaped_path != NULL)
+		{
+			if (ms_replace_joined_str(&path_string, escaped_path) != NULL)
+			{
+				if (ms_replace_joined_str(&path_string, "'") != NULL)
+					ms_perror_cmd2("cd", path_string, strerror(errno));
+			}
+			free(escaped_path);
+		}
+		free(path_string);
+	}
+}
+
+static bool	ms_is_should_escape(const char *path)
+{
+	while (*path != '\0')
+	{
+		if (ft_isprint(*path) == 0)
+			return (true);
+		path++;
+	}
+	return (false);
 }
