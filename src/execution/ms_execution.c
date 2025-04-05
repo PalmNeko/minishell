5521ffd1e16c373eeeb4a942d7724356fb59b77f
nytsdayo <nyantsutsu@gmail.com>
2025-04-05 17:43:22 +0900

    fix : for #215 (#216)

--- src/execution/ms_execution.c
+++ src/execution/ms_execution.c
@@ -6,7 +6,7 @@
 /*   By: rnakatan <rnakatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 21:33:59 by rnakatan          #+#    #+#             */
-/*   Updated: 2025/04/05 13:04:56 by rnakatan         ###   ########.fr       */
+/*   Updated: 2025/04/05 16:24:04 by rnakatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,9 @@
 #include "libms.h"
 #include "semantic_analyze.h"
 #include "syntax_analyze.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-/*notes
- * must add ENOMEM case process.
- */
+static void	ms_destroy_tokens(t_token **tokens);
 
 int	ms_execution(const char *input)
 {
@@ -31,22 +28,26 @@ int	ms_execution(const char *input)
 
 	ret = 0;
 	tokens = ms_lexical_analyze(input);
-	if (tokens[0] != NULL)
-	{
-		node = ms_syntax_analyze(tokens);
-		if (node->type != SY_DECLINED)
-		{
-			lsa = semantic_analyze(node);
-			ret = ms_execute_from_lsa(lsa);
-			ms_lsa_destroy(lsa);
-		}
-		else
-		{
-			ret = 2;
-			ms_set_exit_status(ret);
-		}
-		ms_syntax_node_destroy(node);
-	}
-	ms_destroy_ntp2((void **)tokens, ms_destroy_token_wrapper);
+	if (tokens == NULL || tokens[0] == NULL)
+		return (ms_destroy_tokens(tokens), 1);
+	node = ms_syntax_analyze(tokens);
+	if (node == NULL)
+		return (ms_destroy_tokens(tokens), 1);
+	if (node->type == SY_DECLINED)
+		return (ms_syntax_node_destroy(node),
+			ms_destroy_tokens(tokens), ms_set_exit_status(2), 2);
+	lsa = semantic_analyze(node);
+	if (lsa == NULL)
+		return (ms_syntax_node_destroy(node),
+			ms_destroy_tokens(tokens), ms_set_exit_status(1), 1);
+	ret = ms_execute_from_lsa(lsa);
+	ms_lsa_destroy(lsa);
+	ms_syntax_node_destroy(node);
+	ms_destroy_tokens(tokens);
 	return (ret);
+}
+
+static void	ms_destroy_tokens(t_token **tokens)
+{
+	ms_destroy_ntp2((void **)tokens, ms_destroy_token_wrapper);
 }
