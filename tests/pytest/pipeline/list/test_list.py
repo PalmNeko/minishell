9bf29f05463fbdc1020dc13bae345e23da6f3a74
nytsdayo <nyantsutsu@gmail.com>
2025-04-07 22:06:21 +0900

    add test: #252

--- tests/pytest/pipeline/list/test_list.py
+++ tests/pytest/pipeline/list/test_list.py
@@ -11,3 +11,10 @@ def test_list_status():
 		expected_stderr = "minishell: not_such_command: command not found\n"
     					"minishell: ahog: command not found\n"
 	)
+
+def test_list_complex():
+	result = "1\n2\n5\n"
+	script_tester.test("pipeline/list/test_list_complex.sh",
+		expected_stdout = result,
+		expected_stderr = "minishell: acho: command not found\n"
+	)
